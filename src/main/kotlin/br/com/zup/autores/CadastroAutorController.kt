ca3399ffcdac7f70779a51e7994ee1ca87620b46
matheusferro <matheussouzaferro@gmail.com>
2021-03-10 09:34:55 -0300

    2-Adicionando validacoes

--- src/main/kotlin/br/com/zup/autores/CadastroAutorController.kt
+++ src/main/kotlin/br/com/zup/autores/CadastroAutorController.kt
@@ -3,12 +3,15 @@ package br.com.zup.autores
 import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Post
+import io.micronaut.validation.Validated
+import javax.validation.Valid
 
+@Validated
 @Controller("/api/v1/autor")
 class CadastroAutorController {
 
     @Post
-    fun cadastro(@Body request: CadastroAutorRequest){
+    fun cadastro(@Body @Valid request: CadastroAutorRequest){
         println(request)
     }
 }
