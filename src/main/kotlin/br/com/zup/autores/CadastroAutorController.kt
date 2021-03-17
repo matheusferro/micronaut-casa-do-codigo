ec3b3806093ea717aa357e84cf290d5aed58c623
matheusferro <matheussouzaferro@gmail.com>
2021-03-16 21:25:49 -0300

    adicionado retorno ao cadastrar autor.

--- src/main/kotlin/br/com/zup/autores/CadastroAutorController.kt
+++ src/main/kotlin/br/com/zup/autores/CadastroAutorController.kt
@@ -1,8 +1,10 @@
 package br.com.zup.autores
 
+import io.micronaut.http.HttpResponse
 import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Post
+import io.micronaut.http.uri.UriBuilder
 import io.micronaut.validation.Validated
 import javax.transaction.Transactional
 import javax.validation.Valid
@@ -13,10 +15,15 @@ class CadastroAutorController(val autorRepository: AutorRepository) {
 
     @Post
     @Transactional
-    fun cadastro(@Body @Valid request: CadastroAutorRequest){
+    fun cadastro(@Body @Valid request: CadastroAutorRequest): HttpResponse<Any> {
         println("Autor request: ${request}")
         val autor: Autor = request.paraAutor()
         println("Autor modelo: ${autor.nome}")
+
         autorRepository.save(autor)
+
+        val uri = UriBuilder.of("/api/v1/autores/{id}").expand(mutableMapOf(Pair("id", autor.id)))
+
+        return HttpResponse.created(uri)
     }
 }
